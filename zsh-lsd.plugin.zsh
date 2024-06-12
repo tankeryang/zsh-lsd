858b16c6c4abfea34621051574f703a8a7e919b8
tankeryang <youngzyang@outlook.com>
2024-06-12 17:03:48 +0800

    feat: use k

--- zsh-lsd.plugin.zsh
+++ zsh-lsd.plugin.zsh
@@ -8,10 +8,10 @@ fi
 
 ness_params="--group-directories-first --header --hyperlink=auto --date='+%a %Y-%m-%d %H:%M:%S '"
 # Create alias override commands using 'lsd'
-alias l="lsd -lah $ness_params"
-alias ls="lsd $ness_params"
-alias ll="lsd -l $ness_params"
-alias la="lsd -la $ness_params"
-alias lt="lsd -lat $ness_params"
-alias lx="lsd -laX $ness_params"
-alias tree="lsd -l --tree --depth=2 $ness_params"
+alias k="lsd -lah $ness_params"
+alias ks="lsd $ness_params"
+alias kl="lsd -l $ness_params"
+alias ka="lsd -la $ness_params"
+alias kt="lsd -lat $ness_params"
+alias kx="lsd -laX $ness_params"
+alias ktree="lsd -l --tree --depth=2 $ness_params"
